f0ce0eb3c0508f1885ef0cdb7294ea3f9cefdf43
TS <tamir2341@gmail.com>
2023-06-09 01:08:54 +0300

    Added put to update houses route + some clean code

--- main.js
+++ main.js
@@ -23,9 +23,30 @@ app.get('/api/houses', async (req, res) => {
 
 
 app.get('/api/houses/:id', async (req, res) => {
-    const requestedId = req.params.id;
-    const requestedHouse = await House.findAll({where: {id: requestedId}});
+    const requestedHouseId = req.params.id;
+    const requestedHouse = await House.findAll({ where: { id: requestedHouseId } });
     res.json(requestedHouse);
+});
+
+
+app.put('/api/houses/:id', async (req, res) => {
+    // TODO: What if the house doesn't exist?
+    const houseIdToUpdate = req.params.id;
+    const houseToUpdate = req.body;
+    await House.update(
+        {
+            address: houseToUpdate.address,
+            currentValue: houseToUpdate.currentValue,
+            loanAmount: houseToUpdate.loanAmount,
+            risk: houseToUpdate.risk,
+        },
+        {
+            where: {
+                id: houseIdToUpdate 
+            }
+        }
+    );
+    res.status(200).send(`House ${houseIdToUpdate} updated successfully!`);
 });
 
 app.post('/api/houses', async (req, res, next) => {
@@ -39,13 +60,13 @@ app.post('/api/houses', async (req, res, next) => {
             risk: houseToInsert.risk,
         });
     } catch (error) {
-        if(error.name === UNIQUE_ERROR_NAME) {
-            return res.status(500).send(`Can't create object with id ${houseToInsert.id} since it already exists`);
+        if (error.name === UNIQUE_ERROR_NAME) {
+            return res.status(500).send(`Can't create object with id ${houseToInsert.id} since it already exists.`);
         } else {
             next(error);
         }
     }
-    res.json(req.body);
+    res.status(200).send(`House ${houseToInsert.id} created successfully!`);
 });
 
 app.listen(PORT, () => {
