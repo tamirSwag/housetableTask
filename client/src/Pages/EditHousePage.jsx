2e20dbc218ebe0e53ae2959fd42ca1096d3181e0
TS <tamir2341@gmail.com>
2023-06-10 21:14:27 +0300

    clarify that back to home on edit page doesn't save

--- client/src/Pages/EditHousePage.jsx
+++ client/src/Pages/EditHousePage.jsx
@@ -37,7 +37,7 @@ function EditHousePage() {
 
     return (
         <div className="m-2">
-            <Link to={`/`} className="btn btn-secondary btn-sm">Go to home page</Link>
+            <Link to={`/`} className="btn btn-secondary btn-sm">Go to home page (without saving!)</Link>
             <h3>Change the details of your house:</h3>
             <HouseEditForm handleSubmit={handleSubmit} submitBtnText="Submit edit" initialHouse={houseDetails} />
         </div>
