d8a928cd8cd50aeafcf8bcb9e7d5b08cd32d086e
TS <tamir2341@gmail.com>
2023-06-10 14:29:42 +0300

    Added basic react routing

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 import App from './App';
+import NewHouseFormPage from './NewHouseFormPage/NewHouseFormPage';
+import ErrorPage from './ErrorPage';
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import './index.css';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <ErrorPage />
+  },
+  {
+    path: "/new-house",
+    element: <NewHouseFormPage />,
+  }
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
